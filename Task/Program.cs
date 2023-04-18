24bd98ffa71d24dc815f7fc1e741399772d60fa8
NikolaiAbramov <abramovn85@mail.ru>
2023-04-18 18:46:13 +0700

    Создал папку Task, в ней решение задачи

---
+++ Task/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк: ");
+int n = int.Parse(Console.ReadLine()!);
+string[] arr = new string[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите строку {i + 1}: ");
+    arr[i] = Console.ReadLine()!;
+}
+Console.WriteLine($"Начальный массив: [{string.Join(", ", arr)}] \t");
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        count++;
+    }
+}
+string[] newArr = new string[count];
+int j = 0;
+for (int i = 0; i < n; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        newArr[j] = arr[i];
+        j++;
+    }
+}
+Console.WriteLine($"Новый массив с длинной строк <= 3: [{string.Join(", ", newArr)}] \t");
